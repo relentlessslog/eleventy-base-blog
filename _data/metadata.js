d85fb20280ce17f5f9a892f329fa9aad3057dde4
relentlessslog <relentlessslog@gmail.com>
2023-04-06 10:56:28 -0600

    I dunno what this means

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	"language": "en",
 	"description": "I am writing about my experiences as a naval navel-gazer.",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
+		"name": "Michael Hyland",
+		"email": "relentlessslog@gmail.com",
 		"url": "https://example.com/about-me/"
 	}
 }
